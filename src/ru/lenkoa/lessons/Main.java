a48cc2f35a1e36c423ed9d3eee68cf53f82090a1
Elena Andryunina <miranel@gmail.com>
2020-10-13 05:32:20 +0300

    plus boolean

--- src/ru/lenkoa/lessons/Main.java
+++ src/ru/lenkoa/lessons/Main.java
@@ -9,6 +9,7 @@ public class Main {
         fillArray2();
         changeArray();
         fillDiagonal(4);
+        isEqual();
 
     }
 
@@ -58,11 +59,48 @@ public class Main {
             arr[i][i] = 1;
             arr[i][SIZE - 1 - i] = 1;
         }
+        for (int[] ints : arr) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+
     }
 
     public static void findMinMax(int SIZE){
-        int[] arr = new int[SIZE]
+        int[] arr = new int[SIZE];
+        int maxElement = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] > maxElement) {
+                maxElement = arr[i];
+            }
+        }
+        int minElement = arr[0];
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i] < maxElement) {
+                maxElement = arr[i];
+            }
+        }
+        System.out.println("Минимальный элемент массива: " + minElement);
+        System.out.println("Максимальный элемент массива: " + maxElement);
+    }
 
+    public static boolean isEqual (){
+        int[] arr = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int sum1 = 0;
+        int sum2 = 0;
+        do {
+            for (int i = 0; i < arr.length - 1; i++) {
+                for (int j = 0; j <= i; j++) {
+                    sum1 += arr[j];
+                }
+                for (int j = arr.length - 1; j > i; j--) {
+                    sum2 +=arr[j];
+                }
+            }
+        } while (sum1 < sum2);
+        return sum1 == sum2;
     }
 
 
