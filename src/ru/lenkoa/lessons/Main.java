cf5f1fd005b618496e1fd3260146550776a5ad45
Elena Andryunina <miranel@gmail.com>
2020-10-13 03:40:42 +0300

    toString

--- src/ru/lenkoa/lessons/Main.java
+++ src/ru/lenkoa/lessons/Main.java
@@ -1,8 +1,14 @@
 package ru.lenkoa.lessons;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
         invertArray();
+        fillArray();
+        fillArray2();
+        changeArray();
+        fillDiagonal(4);
 
     }
 
@@ -15,6 +21,7 @@ public class Main {
                 arr[i] = 0;
             }
         }
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void fillArray(){
@@ -22,6 +29,7 @@ public class Main {
         for (int i = 0; i < arr.length; i++){
             arr[i] = i * 3;
         }
+        System.out.println(Arrays.toString(arr));
     }
 
     public static void fillArray2(){
@@ -30,7 +38,36 @@ public class Main {
         for (int i = 1; i < arr.length; i++){
             arr[i] = arr[i-1] + 3;
         }
+        System.out.println(Arrays.toString(arr));
     }
+
+    public static void changeArray() {
+        int[] w = { 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
+        int checkArray = 6;
+        for (int i = 0; i < w.length; i++) {
+            if (w[i] < checkArray) {
+                w[i] = w[i] * 2;
+            }
+        }
+        System.out.println(Arrays.toString(w));
+    }
+
+    public static void fillDiagonal(int SIZE){
+        int[][] arr = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++){
+            arr[i][i] = 1;
+            arr[i][SIZE - 1 - i] = 1;
+        }
+    }
+
+    public static void findMinMax(int SIZE){
+        int[] arr = new int[SIZE]
+
+    }
+
+
+
+
 }
 
 
