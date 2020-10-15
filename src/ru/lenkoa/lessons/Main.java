f1f17644f9a1522224c69312b15d11fe95228170
Elena Andryunina <miranel@gmail.com>
2020-10-16 01:11:35 +0300

    Теперь строки красивенько.) Но фигня с нечисловым вводом.

--- src/ru/lenkoa/lessons/Main.java
+++ src/ru/lenkoa/lessons/Main.java
@@ -1,6 +1,5 @@
 package ru.lenkoa.lessons;
 
-import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,19 +8,19 @@ public class Main {
     static Scanner in = new Scanner(System.in);
     static Random random = new Random();
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         randomGame();
     }
 
     private static void randomGame() {
         int test = random.nextInt(10);
+        System.out.println("Угадайте число");
 
         for (int i = 1; i < 4; i++) {
-            System.out.println("Угадайте число");
-            System.out.println("\nВведите целое число от 0 до 9. У вас три попытки. \nПопытка " + i + ":");
+            System.out.println("Введите целое число от 0 до 9. У вас три попытки. \nПопытка " + i + ":");
             int a = in.hasNextInt() ? in.nextInt() : 10;
             if (a < 0 || a > 9) {
-                System.out.println("Нужно было ввести число от 0 до 9");
+                System.out.println("Вы ввели не число от 0 до 9");
             } else if (a == test) {
                 System.out.println("Поздравляю, вы угадали правильно!");
                 break;
