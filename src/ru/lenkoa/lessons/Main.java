895a5d6879e5f968b6965a2ead82ec81a03b73d7
Elena Andryunina <miranel@gmail.com>
2020-10-19 23:50:59 +0300

    initial commit for lesson 4 homework Код с урока, не свой

--- src/ru/lenkoa/lessons/Main.java
+++ src/ru/lenkoa/lessons/Main.java
@@ -5,50 +5,184 @@ import java.util.Scanner;
 
 public class Main {
 
-    static Scanner in = new Scanner(System.in);
+    static final int SIZE = 3;
+
+
+    static final char DOT_EMPTY = '•';
+    static final char DOT_HUMAN = 'X';
+    static final char DOT_AI = 'O';
+
+    static final String HEADER_FIRST_EMPTY = "♥";
+    static final String EMPTY = " ";
+
+
+    static char[][] map = new char[SIZE][SIZE];
+    static Scanner scanner = new Scanner(System.in);
     static Random random = new Random();
 
+
     public static void main(String[] args) {
-        randomGame();
+        turnGame();
+
     }
 
-    private static void randomGame() {
-        int test = random.nextInt(10);
-        System.out.println("Угадайте число");
-
-        for (int i = 1; i < 4; i++) {
-            System.out.println("Введите целое число от 0 до 9. У вас три попытки. \nПопытка " + i + ":");
-            int a;
-            if (in.hasNextInt()) {
-                a = in.nextInt();
-            } else {
-                System.out.println("Вы ввели не число от 0 до 9");
-                in.next();
-                continue;
-            }
-
-            if (a < 0 || a > 9) {
-                System.out.println("Вы ввели не число от 0 до 9");
-            } else if (a == test) {
-                System.out.println("Поздравляю, вы угадали правильно!");
-                break;
-            } else if (a < test) {
-                System.out.println("Вы ввели слишком маленькое число");
-            } else {
-                System.out.println("Вы ввели слишком большое число");
-            }
-        }
-
-        tryAgain();
+    private static void turnGame() {
+        initMap();
+        printMap();
+        playGame();
     }
 
-    private static void tryAgain() {
-        System.out.println("Повторить игру ещё раз? 1 – да / 0 – нет");
-
-        switch (in.next()) {
-            case "0", "нет" -> System.exit(0);
-            case "1", "да" -> randomGame();
+    private static void initMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
         }
+    }
+
+    private static void printMap() {
+        printMapHeader();
+        printMapRow();
+    }
+
+    private static void printMapHeader() {
+        System.out.print(HEADER_FIRST_EMPTY + EMPTY);
+        for (int i = 0; i < SIZE; i++) {
+            printMapNumber(i);
+        }
+        System.out.println();
+    }
+
+    private static void printMapNumber(int i) {
+        System.out.print(i + 1 + EMPTY);
+    }
+
+    private static void printMapRow() {
+        for (int i = 0; i < SIZE; i++) {
+            printMapNumber(i);
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + EMPTY);
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static void playGame() {
+
+        while (true) {
+            humanTurn();
+            printMap();
+            checkEnd(DOT_HUMAN);
+
+
+            aiTurn();
+            printMap();
+            checkEnd(DOT_AI);
+        }
+    }
+
+    private static void humanTurn() {
+        int rowNumber;
+        int colNumber;
+
+        System.out.println("\nХод человека! Введите номер строки и столбца!");
+        do {
+            System.out.print("Строка = ");
+            rowNumber = scanner.nextInt();
+            System.out.print("Столбец = ");
+            colNumber = scanner.nextInt();
+        } while (!isCellValid(rowNumber, colNumber));
+
+        map[rowNumber - 1][colNumber - 1] = DOT_HUMAN;
+    }
+
+    private static boolean isCellValid(int rowNumber, int colNumber, boolean isAI) {
+
+        if (!isAI && ((rowNumber < 1) || (rowNumber > SIZE) || (colNumber < 1) || (colNumber > SIZE))) {
+            System.out.println("\nПроверьте значения строки и столбца");
+            return false;
+        }
+
+        if (map[rowNumber - 1][colNumber - 1] != DOT_EMPTY) {
+            if (!isAI) {
+                System.out.println("\nВы выбрали занятую ячейку");
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isCellValid(int rowNumber, int colNumber) {
+        return isCellValid(rowNumber, colNumber, false);
+    }
+
+    private static void checkEnd(char symbol) {
+
+        boolean isEnd = false;
+
+        if(checkWin(symbol)) {
+            String winMessage;
+
+            if(symbol == DOT_HUMAN) {
+                winMessage = "УРА! Вы победили!";
+            }
+            else {
+                winMessage = "Восстание близко! AI победил";
+            }
+
+            isEnd = true;
+            System.out.println(winMessage);
+        }
+
+        if(!isEnd && isMapFull()) {
+            System.out.println("Ничья!");
+            isEnd = true;
+        }
+
+        if(isEnd) {
+            System.exit(0);
+        }
+    }
+
+    private static boolean checkWin(char symbol) {
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
+        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
+        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
+
+        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
+        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
+        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
+
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (map[0][2] == symbol && map[1][1] == symbol && map[2][0] == symbol) return true;
+
+        return false;
+    }
+
+    private static boolean isMapFull() {
+        for (char[] chars : map) {
+            for (char aChar : chars) {
+                if(aChar == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+    private static void aiTurn() {
+        int rowNumber;
+        int colNumber;
+        System.out.println("\nХод компьютера!\n");
+        do {
+            rowNumber = random.nextInt(SIZE) + 1;
+            colNumber = random.nextInt(SIZE) + 1;
+        } while (!isCellValid(rowNumber, colNumber, true));
+
+        map[rowNumber - 1][colNumber - 1] = DOT_AI;
     }
 
 }
