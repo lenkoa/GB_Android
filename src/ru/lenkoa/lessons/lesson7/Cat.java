c237b9b5e158c73614cf8a464d38194832bcdd9f
Elena Andryunina <miranel@gmail.com>
2020-11-06 01:04:26 +0300

    С именами котов и проверкой на достаточность еды

--- src/ru/lenkoa/lessons/lesson7/Cat.java
+++ src/ru/lenkoa/lessons/lesson7/Cat.java
@@ -10,18 +10,21 @@ public class Cat {
     }
 
     public void eat(Plate plate) {
-        System.out.println("Котик питается");
+        System.out.println("Котик " + name + "питается");
 
         if (plate.getFood() >= appetite) {
             doEat(plate);
-            System.out.println("Кот доволен");
+            System.out.println("Кот " + name + " доволен");
+        }
+        else if(plate.getFood() < appetite && plate.getFood() > 0){
+            doEatPartially(plate);
+            System.out.println("Кот " + name + " не наелся");
         }
         else {
-            System.out.println("Мне нужна еда!");
+            System.out.println("Коту " + name + "нужна еда!");
         }
     }
 
-    private void doEat(Plate plate) {
-        plate.decreaseFood(appetite);
-    }
+    private void doEat(Plate plate) { plate.decreaseFood(appetite); }
+    private void doEatPartially(Plate plate) { plate.decreaseFood(plate.getFood()); }
 }
