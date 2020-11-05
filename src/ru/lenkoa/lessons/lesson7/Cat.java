811fc2676510d69e675f57d7b236297e38836fae
Elena Andryunina <miranel@gmail.com>
2020-11-06 01:14:40 +0300

    С полем сытость

--- src/ru/lenkoa/lessons/lesson7/Cat.java
+++ src/ru/lenkoa/lessons/lesson7/Cat.java
@@ -3,10 +3,12 @@ package ru.lenkoa.lessons.lesson7;
 public class Cat {
     private String name;
     private int appetite;
+    private boolean fullness;
 
     public Cat(String name, int appetite) {
         this.appetite = appetite;
         this.name = name;
+        this.fullness = false;
     }
 
     public void eat(Plate plate) {
@@ -15,10 +17,12 @@ public class Cat {
         if (plate.getFood() >= appetite) {
             doEat(plate);
             System.out.println("Кот " + name + " доволен");
+            fullness = true;
         }
         else if(plate.getFood() < appetite && plate.getFood() > 0){
             doEatPartially(plate);
             System.out.println("Кот " + name + " не наелся");
+            appetite -= plate.getFood();
         }
         else {
             System.out.println("Коту " + name + "нужна еда!");
