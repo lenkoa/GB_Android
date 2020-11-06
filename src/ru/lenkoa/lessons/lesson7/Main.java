e3ba699208750b0f025f40def7c45458f6fe1bd7
Elena Andryunina <miranel@gmail.com>
2020-11-06 18:53:49 +0300

    С полем сытость, массивом котов и массивом тарелок

--- src/ru/lenkoa/lessons/lesson7/Main.java
+++ src/ru/lenkoa/lessons/lesson7/Main.java
@@ -1,14 +1,49 @@
 package ru.lenkoa.lessons.lesson7;
+// решение на одну итерацию, без повторений
+import java.util.Random;
 
 public class Main {
-    public static void main(String[] args) {
-        Cat cat = new Cat("Борис Николаевич", 3);
-        Plate plate = new Plate(5);
+    public static Random random = new Random();
+    public static int plateCapacity = 5;
 
-        plate.addFood(4);
-        plate.printInfo();
-        cat.eat(plate);
-        plate.printInfo();
+    public static void main(String[] args) {
+
+        Cat[] cats = new Cat[4];
+        cats[0] = new Cat("Борис Николаевич", 3);
+        cats[1] = new Cat("Бонифаций Тайсюшевич", 4);
+        cats[2] = new Cat("Бенито Муссолини", 2);
+        cats[3] = new Cat("Варвара Богдансдоттир", 3);
+
+        Plate[] plates = new Plate[4];
+        plates[0] = new Plate(plateCapacity, 0);
+        plates[1] = new Plate(plateCapacity, 0);
+        plates[2] = new Plate(plateCapacity, 0);
+        plates[3] = new Plate(plateCapacity, 0);
+
+
+        for (int i = 0; i < plates.length; i++) { // заполняем тарелки случайным количеством еды
+            plates[i].addFood(random.nextInt(plateCapacity + 1));
+        }
+
+        for (Plate plate: plates // выводим информацию о тарелках в консоль
+             )
+            plate.printInfo();
+
+        for (Cat cat: cats // кормим котов
+             ) {
+            for (Plate plate: plates
+                 )
+                cat.eat(plate);
+        }
+
+        for (Plate plate: plates // выводим информацию о тарелках в консоль после еды
+        ) {
+            plate.printInfo();
+        }
+
 
     }
+
+
 }
+
