08bf51bc84056624cba81fd068832958477db102
Elena Andryunina <miranel@gmail.com>
2020-12-04 20:50:32 +0300

    Готово. ДЗ к уроку 2_5

--- src/ru/lenkoa/lessons2/lesson5/Main.java
+++ src/ru/lenkoa/lessons2/lesson5/Main.java
@@ -1,4 +1,45 @@
 package ru.lenkoa.lessons2.lesson5;
 
+import java.util.stream.IntStream;
+
 public class Main {
+    static final int SIZE = 10000000;
+    static final int h = SIZE / 2;
+
+    public static void main (String[] args){
+        Main main = new Main();
+        main.countA();
+        main.countB();
+    }
+
+    public void countA(){
+        float[] arr = new float[SIZE];
+        IntStream.range(0, arr.length).forEach(i -> arr[i] = 1);
+        long a = System.currentTimeMillis();
+        IntStream.range(0, arr.length).forEach(i -> arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public void countB(){
+        float[] arr = new float[SIZE];
+        IntStream.range(0, arr.length).forEach(i -> arr[i] = 1);
+        long a = System.currentTimeMillis();
+        Thread t1 = new Thread(() -> {
+            float[] a1 = new float[h];
+            System.arraycopy(arr, 0, a1, 0, h);
+            IntStream.range(0, a1.length).forEach(i -> a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+            synchronized(this) {System.arraycopy(a1, 0, arr, 0, h);}
+        });
+        Thread t2 = new Thread(() -> {
+            float[] a2 = new float[h];
+            System.arraycopy(arr, h, a2, 0, h);
+            IntStream.range(0, a2.length).forEach(i -> a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+            synchronized(this) {System.arraycopy(a2, 0, arr, h, h);}
+        });
+
+        t1.start();
+        t2.start();
+
+        System.out.println(System.currentTimeMillis() - a);
+    }
 }
