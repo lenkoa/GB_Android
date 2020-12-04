e7c8293d7666d03383e9a6f45cf1aebe4a5b589f
Elena Andryunina <miranel@gmail.com>
2020-12-04 22:01:49 +0300

    А не, не готово, ерунду творит. В процессе...

--- src/ru/lenkoa/lessons2/lesson5/Main.java
+++ src/ru/lenkoa/lessons2/lesson5/Main.java
@@ -8,38 +8,41 @@ public class Main {
 
     public static void main (String[] args){
         Main main = new Main();
-        main.countA();
+        // main.countA();
         main.countB();
+
     }
 
-    public void countA(){
-        float[] arr = new float[SIZE];
-        IntStream.range(0, arr.length).forEach(i -> arr[i] = 1);
+    public synchronized void countA(){
+        float[] arr1 = new float[SIZE];
+        IntStream.range(0, arr1.length).forEach(i -> arr1[i] = 1);
+        System.out.println("Время создания массива 1: " + System.currentTimeMillis());
         long a = System.currentTimeMillis();
-        IntStream.range(0, arr.length).forEach(i -> arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-        System.out.println(System.currentTimeMillis() - a);
+        IntStream.range(0, arr1.length).forEach(i -> arr1[i] = (float)(arr1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+        System.out.println("Время обработки массива 1: " + (System.currentTimeMillis() - a));
     }
 
-    public void countB(){
+    public synchronized void countB(){
         float[] arr = new float[SIZE];
         IntStream.range(0, arr.length).forEach(i -> arr[i] = 1);
+        System.out.println("Время создания массива 2: " + System.currentTimeMillis());
         long a = System.currentTimeMillis();
         Thread t1 = new Thread(() -> {
             float[] a1 = new float[h];
             System.arraycopy(arr, 0, a1, 0, h);
             IntStream.range(0, a1.length).forEach(i -> a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-            synchronized(this) {System.arraycopy(a1, 0, arr, 0, h);}
+            synchronized(arr) {System.arraycopy(a1, 0, arr, 0, h);}
         });
         Thread t2 = new Thread(() -> {
             float[] a2 = new float[h];
             System.arraycopy(arr, h, a2, 0, h);
             IntStream.range(0, a2.length).forEach(i -> a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
-            synchronized(this) {System.arraycopy(a2, 0, arr, h, h);}
+            synchronized(arr) {System.arraycopy(a2, 0, arr, h, h);}
         });
 
         t1.start();
         t2.start();
 
-        System.out.println(System.currentTimeMillis() - a);
+        System.out.println("Время обработки массива 2: " + (System.currentTimeMillis() - a));
     }
 }
