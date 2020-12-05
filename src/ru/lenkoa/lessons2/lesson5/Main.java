6a8e811c7408310c0cedf037019550fbd4ce0724
Elena Andryunina <miranel@gmail.com>
2020-12-05 18:09:56 +0300

    Исправила подсчет значений во втором массиве

--- src/ru/lenkoa/lessons2/lesson5/Main.java
+++ src/ru/lenkoa/lessons2/lesson5/Main.java
@@ -5,12 +5,12 @@ import java.util.stream.IntStream;
 public class Main {
     static final int SIZE = 10000000;
     static final int h = SIZE / 2;
+    static final int h2 = SIZE - h; // на случай нечетного количества элементов в массиве
 
     public static void main (String[] args){
         Main main = new Main();
         // main.countA();
         main.countB();
-
     }
 
     public synchronized void countA(){
@@ -30,13 +30,17 @@ public class Main {
         Thread t1 = new Thread(() -> {
             float[] a1 = new float[h];
             System.arraycopy(arr, 0, a1, 0, h);
-            IntStream.range(0, a1.length).forEach(i -> a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+            for (int i = 0; i < a1.length; i++) {
+                a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
             synchronized(arr) {System.arraycopy(a1, 0, arr, 0, h);}
         });
         Thread t2 = new Thread(() -> {
-            float[] a2 = new float[h];
-            System.arraycopy(arr, h, a2, 0, h);
-            IntStream.range(0, a2.length).forEach(i -> a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2)));
+            float[] a2 = new float[h2];
+            System.arraycopy(arr, h, a2, 0, h2);
+            for (int i = h; i < SIZE; i++) {
+                a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
             synchronized(arr) {System.arraycopy(a2, 0, arr, h, h);}
         });
 
